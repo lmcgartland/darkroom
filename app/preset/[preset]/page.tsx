0693fd0cf2d1446b079a5d32a4f7d3d5ffa50000
Lucas McGartland <luke.mcgartland@me.com>
2024-12-15 00:43:43 -0800

    2 col layout

--- app/preset/[preset]/page.tsx
+++ app/preset/[preset]/page.tsx
@@ -13,11 +13,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function GalleryPage(
-  props: {
-    params: Promise<{ preset: string }>;
-  }
-) {
+export default async function GalleryPage(props: {
+  params: Promise<{ preset: string }>;
+}) {
   const params = await props.params;
   const { preset } = params;
   const presetDir = path.join(process.cwd(), "public", "presets", preset);
@@ -29,28 +27,27 @@ export default async function GalleryPage(
 
   return (
     <div className="p-4 sm:p-8 md:p-12 lg:p-16 font-[family-name:var(--font-geist-mono)]">
-      <main className="flex flex-col justify-center items-center gap-8 w-full max-w-6xl mx-auto">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl text-center sm:text-left font-[family-name:var(--font-geist-mono)] mb-4">
-          {preset}
-        </h1>
-
-        <div className="w-full max-w-2xl">
+      <main className="flex flex-col lg:flex-row justify-center gap-8 w-full max-w-6xl mx-auto align-center">
+        <div className="w-full lg:w-1/3">
           <DraggableGallery
             beforeImage={originalImage}
             afterImage={editedImage}
             alt={`Before and after for ${preset}`}
           />
         </div>
-
-        <ExifData data={exifData} />
-
-        <a
-          href={`/presets/${preset}/${preset}.xmp`}
-          download={`${preset}.xmp`}
-          className="font-[family-name:var(--font-geist-mono)] flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white py-2 px-5 rounded-full"
-        >
-          Download XMP
-        </a>
+        <div className="w-full lg:w-1/3 flex flex-col gap-8">
+          <h1 className="text-xl sm:text-3xl md:text-3xl text-center sm:text-left font-[family-name:var(--font-geist-mono)] mb-1">
+            {preset}
+          </h1>
+          <ExifData data={exifData} />
+          <a
+            href={`/presets/${preset}/${preset}.xmp`}
+            download={`${preset}.xmp`}
+            className="font-[family-name:var(--font-geist-mono)] flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white py-2 px-5 rounded-full self-start"
+          >
+            Download XMP
+          </a>
+        </div>
       </main>
     </div>
   );
